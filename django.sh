9575f9c110ca485b614958e8044a4b6ac788e0bb
abhay <abhay@gmail.com>
2023-09-29 17:19:05 +0530

    a

--- django.sh
+++ django.sh
@@ -4,12 +4,14 @@ while [ ! -f /tmp/public_ip ]; do sleep 10; done
 echo 'File is available, continuing with setup...'
 sudo apt update -y
 sudo apt install python3-pip -y
+sudo apt update -y
 sudo apt install python3-virtualenv -y
-cd /home/ubuntu
 sudo virtualenv connectrdj
 source connectrdj/bin/activate
-sud pip install django
+sudo pip install django
 sudo pip install djangorestframework
+sudo pip install django-cors-headers
+sudo apt-get install libpq-dev -y
 sudo pip install psycopg2
 sudo git clone https://github.com/abhay9175/djangobackend.git /home/ubuntu/my-git-repo 2>> /home/ubuntu/git_error.log
 word_to_replace=$(cat /tmp/public_ip)
